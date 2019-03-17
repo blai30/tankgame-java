2cc2e9c750ceb71a0989f996890b72150095d11a
Brian Lai <brianlai30@outlook.com>
2019-03-16 19:45:31 -0700

    Added gitignore and src

---
+++ src/TRE.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tankrotationexample;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import java.util.Objects;
+
+import static javax.imageio.ImageIO.read;
+
+/**
+ *
+ * @author anthony-pc
+ */
+public class TRE extends JPanel  {
+
+
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 960;
+    private BufferedImage world;
+    private Graphics2D buffer;
+    private JFrame jf;
+    private Tank t1;
+
+
+    public static void main(String[] args) {
+        Thread x;
+        TRE trex = new TRE();
+        trex.init();
+        try {
+
+            while (true) {
+                trex.t1.update();
+                trex.repaint();
+                System.out.println(trex.t1);
+                Thread.sleep(1000 / 144);
+            }
+        } catch (InterruptedException ignored) {
+
+        }
+
+    }
+
+
+    private void init() {
+        this.jf = new JFrame("Tank Rotation");
+        this.world = new BufferedImage(TRE.SCREEN_WIDTH, TRE.SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        BufferedImage t1img = null, t2img = null;
+        try {
+            BufferedImage tmp;
+            System.out.println(System.getProperty("user.dir"));
+            /*
+             * note class loaders read files from the out folder (build folder in netbeans) and not the
+             * current working directory.
+             */
+            t1img = read(new File("tank1.png"));
+
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        t1 = new Tank(200, 200, 0, 0, 0, t1img);
+
+
+        TankControl tc1 = new TankControl(t1, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_ENTER);
+
+        this.jf.setLayout(new BorderLayout());
+        this.jf.add(this);
+
+
+        this.jf.addKeyListener(tc1);
+
+
+        this.jf.setSize(TRE.SCREEN_WIDTH, TRE.SCREEN_HEIGHT + 30);
+        this.jf.setResizable(false);
+        jf.setLocationRelativeTo(null);
+
+        this.jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.jf.setVisible(true);
+
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        buffer = world.createGraphics();
+        super.paintComponent(g2);
+
+        this.t1.drawImage(buffer);
+        g2.drawImage(world,0,0,null);
+
+    }
+
+
+}
---
+++ src/Tank.java
@@ -0,0 +1,154 @@
+package tankrotationexample;
+
+
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author anthony-pc
+ */
+public class Tank{
+
+
+    private int x;
+    private int y;
+    private int vx;
+    private int vy;
+    private int angle;
+
+    private final int R = 2;
+    private final int ROTATIONSPEED = 4;
+
+
+
+    private BufferedImage img;
+    private boolean UpPressed;
+    private boolean DownPressed;
+    private boolean RightPressed;
+    private boolean LeftPressed;
+
+
+    Tank(int x, int y, int vx, int vy, int angle, BufferedImage img) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.img = img;
+        this.angle = angle;
+
+    }
+
+
+    void toggleUpPressed() {
+        this.UpPressed = true;
+    }
+
+    void toggleDownPressed() {
+        this.DownPressed = true;
+    }
+
+    void toggleRightPressed() {
+        this.RightPressed = true;
+    }
+
+    void toggleLeftPressed() {
+        this.LeftPressed = true;
+    }
+
+    void unToggleUpPressed() {
+        this.UpPressed = false;
+    }
+
+    void unToggleDownPressed() {
+        this.DownPressed = false;
+    }
+
+    void unToggleRightPressed() {
+        this.RightPressed = false;
+    }
+
+    void unToggleLeftPressed() {
+        this.LeftPressed = false;
+    }
+
+
+
+    public void update() {
+        if (this.UpPressed) {
+            this.moveForwards();
+        }
+        if (this.DownPressed) {
+            this.moveBackwards();
+        }
+
+        if (this.LeftPressed) {
+            this.rotateLeft();
+        }
+        if (this.RightPressed) {
+            this.rotateRight();
+        }
+
+
+    }
+
+    private void rotateLeft() {
+        this.angle -= this.ROTATIONSPEED;
+    }
+
+    private void rotateRight() {
+        this.angle += this.ROTATIONSPEED;
+    }
+
+    private void moveBackwards() {
+        vx = (int) Math.round(R * Math.cos(Math.toRadians(angle)));
+        vy = (int) Math.round(R * Math.sin(Math.toRadians(angle)));
+        x -= vx;
+        y -= vy;
+        checkBorder();
+    }
+
+    private void moveForwards() {
+        vx = (int) Math.round(R * Math.cos(Math.toRadians(angle)));
+        vy = (int) Math.round(R * Math.sin(Math.toRadians(angle)));
+        x += vx;
+        y += vy;
+        checkBorder();
+    }
+
+
+
+
+    private void checkBorder() {
+        if (x < 30) {
+            x = 30;
+        }
+        if (x >= TRE.SCREEN_WIDTH - 88) {
+            x = TRE.SCREEN_WIDTH - 88;
+        }
+        if (y < 40) {
+            y = 40;
+        }
+        if (y >= TRE.SCREEN_HEIGHT - 80) {
+            y = TRE.SCREEN_HEIGHT - 80;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "x=" + x + ", y=" + y + ", angle=" + angle;
+    }
+
+
+    void drawImage(Graphics g) {
+        AffineTransform rotation = AffineTransform.getTranslateInstance(x, y);
+        rotation.rotate(Math.toRadians(angle), this.img.getWidth() / 2.0, this.img.getHeight() / 2.0);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(this.img, rotation, null);
+    }
+
+
+
+}
