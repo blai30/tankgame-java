0c9c1bef07d9d5777b71d3d9f9fe83b3dba9669a
Brian Lai <brianlai30@outlook.com>
2019-03-19 14:32:18 -0700

    Initialized controls hashmaps in Game.java

--- src/Game.java
+++ src/Game.java
@@ -4,25 +4,38 @@ import util.Key;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.HashMap;
 
 public class Game extends JPanel {
 
     public static final int SCREEN_WIDTH = 1280;
     public static final int SCREEN_HEIGHT = 960;
-    public static HashMap<Integer, Key> controls1;
-    public static HashMap<Integer, Key> controls2;
+
+    private static HashMap<Integer, Key> controls1;
+    private static HashMap<Integer, Key> controls2;
 
     private JFrame screen;
     private Tank tank1;
     private Tank tank2;
 
     private static void setControls() {
-        controls1 = new HashMap<>();
+        controls1.put(KeyEvent.VK_UP, Key.up);
+        controls1.put(KeyEvent.VK_DOWN, Key.down);
+        controls1.put(KeyEvent.VK_LEFT, Key.left);
+        controls1.put(KeyEvent.VK_RIGHT, Key.right);
+        controls1.put(KeyEvent.VK_SLASH, Key.fire);
+
+        controls2.put(KeyEvent.VK_W, Key.up);
+        controls2.put(KeyEvent.VK_S, Key.down);
+        controls2.put(KeyEvent.VK_A, Key.left);
+        controls2.put(KeyEvent.VK_D, Key.right);
+        controls2.put(KeyEvent.VK_F, Key.fire);
     }
 
     private void init() {
         TankController ctrlTank1 = new TankController(tank1, controls1);
+        TankController ctrlTank2 = new TankController(tank2, controls2);
 
         this.screen.setLayout(new BorderLayout());
         this.screen.add(this);
