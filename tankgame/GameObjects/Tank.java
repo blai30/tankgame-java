1f60b957fd6cf7c038bc29e86d99191edb1b0b66
Brian Lai <brianlai30@outlook.com>
2019-04-01 16:59:51 -0700

    Changed aim line so that it reflects the tank's current hp. It gets shorter and changes color like the health bar

--- tankgame/GameObjects/Tank.java
+++ tankgame/GameObjects/Tank.java
@@ -200,11 +200,11 @@ public class Tank extends Player implements SolidObject {
     @Override
     public void drawGizmos(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.GREEN);
+        g2d.setColor((this.currentHP > 5) ? Color.GREEN : (this.currentHP > 2) ? Color.YELLOW : Color.RED);
 
         // Draw aim line
-        float toX = (float) (500 * Math.cos(Math.toRadians(this.transform.getRotation())));
-        float toY = (float) (500 * Math.sin(Math.toRadians(this.transform.getRotation())));
+        float toX = (float) (this.currentHP * 50 * Math.cos(Math.toRadians(this.transform.getRotation())));
+        float toY = (float) (this.currentHP * 50 * Math.sin(Math.toRadians(this.transform.getRotation())));
         g2d.drawLine((int) (this.transform.getPositionX() + this.originOffset.getX()), (int) (this.transform.getPositionY() + this.originOffset.getY()), (int) (this.transform.getPositionX() + this.originOffset.getX() + toX), (int) (this.transform.getPositionY() + this.originOffset.getY() + toY));
     }
 
